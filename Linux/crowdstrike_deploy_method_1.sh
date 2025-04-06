2d10b394399ec9bdb6966721ce249b7e15f6e4d6
Eilay Yosfan (DFIR) <132997318+YosfanEilay@users.noreply.github.com>
2025-04-06 13:39:45 +0300

    update

--- Linux/crowdstrike_deploy_method_1.sh
+++ Linux/crowdstrike_deploy_method_1.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# method: 1
+# method version: v1.1
+# method description: using dropbox to download the sensor and install it on a linux machine
+
 # Crowdstrike-Deploy Banner
 echo ""
 echo "  ██████╗██████╗  ██████╗ ██╗    ██╗██████╗ ███████╗████████╗██████╗ ██╗██╗  ██╗███████╗"
@@ -18,14 +22,15 @@ echo "                     ╚═════╝ ╚══════╝╚═�
 echo ""
 echo "                           Created & Maintained by: Eilay Yosfan"
 echo "                                   GitHub.com/YosfanEilay"
-echo "                                        Version: 1.0"
+echo "                                        Version: 1.1"
+echo "                                          Method: 1"
 echo ""
 
 ###### Please Paste Your Information in Here ######
-SensorLink='' # Crowdstrike Sensor Download Link
-SensorSig1="" # Crowdstrike Sensor Hash (SHA256)
-TenantCID=""  # Crowdstrike Tenant CID
-TenantName="" # Crowdstrike Tenant Name
+SensorLink='https://www.dropbox.com/scl/fi/uvt0g06qzrxoed55649kj/falcon-sensor_7.23.0-17607_amd64.deb?rlkey=gphx5hkwso17v4fk2ggo4si98&st=8sy1mnsf&dl=1' # Crowdstrike Sensor Download Link
+SensorSig1="4CFBCF819C9CF8FD0A0B7150313DEA8538A9B75CE472A35DC67780D434887EFE" # Crowdstrike Sensor Hash (SHA256)
+TenantCID="8D2DAEC5D5D1469788B85B357DFFB298-BA"  # Crowdstrike Tenant CID
+TenantName="Security Joes" # Crowdstrike Tenant Name
 ###################################################
 
 # Prerequisite Variable Load
@@ -47,20 +52,22 @@ else
     exit 1
 fi
 
-# Test Connection to OneDrive
-if ping -c 2 "onedrive.live.com" &> /dev/null; then
-    echo "[+] OneDrive is reachable."
+# Test Connection to Dropbox
+if ping -c 2 "dropbox.com" &> /dev/null; then
+    echo "[+] Dropbox is reachable."
 else
-    echo "[!] OneDrive is not reachable, might be related to host network or organization policy. Deploy might fail."
+    echo "[!] Dropbox is not reachable, might be related to host network or organization policy. Deploy might fail."
 fi
 
 # Download Crowdstrike Sensor
-echo "[+] Download has started. The time required will depend on the host's bandwidth."
+echo "[+] Download has started, the time required will depend on the host's bandwidth."
 echo # Blank Line
 curl -L -o CrowdstrikeSensor.deb -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36" "$SensorLink"
 
 # Check if the Downloaded Sensor File is Corrupted
 SensorSig2=$(openssl sha256 "$DstPath" | awk '{print $2}')
+SensorSig1=${SensorSig1,,}
+SensorSig2=${SensorSig2,,}
 if [ "$SensorSig1" == "$SensorSig2" ]; then
     echo "[+] Crowdstrike sensor was successfully downloaded. Sensor installation started."
 else
@@ -70,16 +77,14 @@ else
 fi
 
 # Start Crowdstrike Installation Process
-echo # Blank Line
-echo "[+] Showing installation process.. [60 seconds]"
+echo "[+] Showing installation process:"
 dpkg -i "CrowdstrikeSensor.deb"
 sleep 60
 /opt/CrowdStrike/falconctl -s --cid="$TenantCID"
 systemctl start falcon-sensor
 
 # show that falcon crowdstrike sensor process is running using ps aux
-echo # Blank Line
-echo "[+] Showing that falcon is indeed running."
+echo "[+] Showing that falcon is indeed running:"
 ps aux | grep "falcon"
 echo # Blank Line
 
