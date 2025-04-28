37db9f34c83852a9faf565a02ea26bae48c1b6ab
Eilay Yosfan (DFIR) <132997318+YosfanEilay@users.noreply.github.com>
2025-04-28 11:19:35 +0300

    update

--- Uninstall/Mac/Method_1/mac_crowdstrike_uninstall_method_1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-expect <<- DONE
-spawn /Applications/Falcon.app/Contents/Resources/falconctl uninstall -t
-expect "Falcon Maintenance Token:"
-send -- "PASTE_TOKEN_IN_HERE"
-send -- "\r"
-expect eof
-DONE
